31f4a23628307b172a8d9eb67dd0ef8513192c65
swikot <swikot@gmail.com>
2020-12-15 17:42:55 +0600

    template engine

--- first-app.js
+++ first-app.js
@@ -1,30 +1,35 @@
+const port = 3000
 //core imports
 const path =require('path');
 
 // Third Party imports
 const express    =require('express');
 const bodyParser =require('body-parser');
+const exphbs =require('express-handlebars');
 
 // local imports 
 const shopRouting=require('./routers/shop');
-const adminRouting=require('./routers/admin');
+const adminData=require('./routers/admin');
 const rootDir= require('./util/path')
-
 //express
 const app = express();
+//setting up template engine 
+app.engine('hbs', exphbs({ defaultLayout: null}));
+app.set('view engine','hbs')
+app.set('views','views')
 //  middleware
 app.use(bodyParser.urlencoded({ extended: false })) //Parsig form data
 app.use(express.static(path.join(rootDir,'public')))//serving static files
 //serving routers 
-app.use('/admin',adminRouting)
+app.use('/admin',adminData)
 app.use(shopRouting) 
 // error page 
 app.use((req,res,next)=>{
-   res.status(404).sendFile(path.join(rootDir,'views','404.html'));
+   res.status(404).render('404',{pageTitle:'Page Not Found'});
 
 })
 //start from here 
-app.listen(3000)
+app.listen(port, () => console.log(`App listening to port ${port}`));
 //finish_port_many
 
 
